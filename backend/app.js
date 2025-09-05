49441d45945bd8891302986b5d34fcbf30dff3d4
Harshita-Sharma-22 <harshita.sharma.thandla@gmail.com>
2025-09-05 17:00:09 +0530

    error

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const app = express()
-
 require("./connection/connection")
+const auth = require("./routes/auth")
+app.use(express.json())
+
 app.get("/", (req,res) => {
     res.send("Hello")
 })
 
+app.use("/api/v1", auth)
 app.listen(1000, ()=>{
     console.log("Server started")
 })
