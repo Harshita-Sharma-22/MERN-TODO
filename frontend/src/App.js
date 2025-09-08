a82401664a11f1b9dd960b41d3b9e75bd21349a2
Harshita-Sharma-22 <harshita.sharma.thandla@gmail.com>
2025-09-09 01:02:41 +0530

    todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import About from './components/about/About'
 import Footer from './components/footer/Footer'
 import Signup from './components/signup/Signup'
 import SignIn from './components/signup/SignIn'
+import Todo from './components/todo/Todo'
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
          <Route exact path='/' element={<Home/>}/>
          <Route path='/about' element={<About/>}/>
+         <Route path='/todo' element={<Todo/>}/>
          <Route path='/signup' element={<Signup/>}/>
          <Route path='/signin' element={<SignIn/>}/>
         </Routes>
---
+++ frontend/src/components/todo/Todo.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './Todo.css'
+import TodoCards from './TodoCards'
+
+
+const Todo = () => {
+    const[Inputs,setInputs] = useState({title:"", body:""})
+    const [Array, setArray] = useState([])
+
+    const show = ()=>{
+        document.getElementById("textarea").style.display="block"
+    }
+
+    const change = (e)=>{
+        const { name, value } = e.target
+        setInputs({...Inputs, [name]: value})
+    }
+    const submit = () =>{
+        setArray([...Array, Inputs])
+        console.log(Inputs)
+        setInputs({title:"", body:""})
+    }
+
+  return (
+    <div className='todo'>
+        <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
+            <div className='d-flex flex-column todo-input-div w-50 p-1'>
+                <input type="text" placeholder='TITLE' className='my-2 p-2 todo-input' name='title' value={Inputs.title} onClick={show} onChange={change}/>
+                <textarea id='textarea' type="text" placeholder='BODY' className=' p-2 todo-input' name='body' value={Inputs.body} onChange={change}/>
+            </div>
+            <div className='w-50 d-flex justify-content-end my-3'>
+                <button className='home-btn px-2 py-1' onClick={submit}>Add</button></div>
+        </div>
+        <div className="todo-body">
+            <div className="container">
+                {Array && 
+                Array.map((item, index) =>(
+                    <>
+                        <TodoCards/>
+                    </>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
